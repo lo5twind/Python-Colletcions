ef490d5e20ede64e02d9a261a114384e4e1d7ca5
lo5twind <lo5twind@foxmail.com>
2016-08-19 10:21:28 +0800

    Django Template test

--- auth/dj_google_auth/dj_google_auth/settings.py
+++ auth/dj_google_auth/dj_google_auth/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'my_google_auth.apps.MyGoogleAuthConfig',
-    'polls',
+    'polls.apps.PollsConfig',
     # 'my_google_auth',
 ]
 
--- auth/dj_google_auth/polls/views.py
+++ auth/dj_google_auth/polls/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 # Create your views here.
 
 
 def index(request):
-    latest_quesgtion_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_quesgtion_list])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list':latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse('Your are looking for question %s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
     response = 'You are looking for results of question %s.'
