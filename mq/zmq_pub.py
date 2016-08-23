8d4ced8e11dc110f3f5353f8577f7eda7a767285
lo5twind <lo5twind@foxmail.com>
2016-08-23 19:13:26 +0800

    zmq pub and sub test

--- mq/zmq_pub.py
+++ mq/zmq_pub.py
@@ -1,16 +1,154 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import sys
 import zmq
 import time
+import json
+import threading
+from contextlib import contextmanager
+from zmq_sub import zmq_req_socket
 
-context = zmq.Context()
-socket = context.socket(zmq.PUB)
-socket.bind("tcp://127.0.0.1:5000")
-count = 0
-while 1:
-    socket.send("zmq send [%s]" % count)
-    count += 1
-    time.sleep(1)
+PUB_POTR = 5000
+REP_POTR = 5001
+ZMQ_ADDR = "tcp://127.0.0.1:{port:}"
+ZMQ_PREFIX = r'ZMQ://'
+ZMQ_MSG = lambda x : ZMQ_PREFIX + x
+ZMQ_EXIT = ZMQ_MSG("{command:'exit'}")
+
+
+def zmq_pub():
+    try:
+        context = zmq.Context()
+        socket = context.socket(zmq.PUB)
+        socket.bind("tcp://127.0.0.1:5000")
+        count = 0
+        while 1:
+            socket.send("zmq send [%s]" % count)
+            count += 1
+            time.sleep(1)
+    except KeyboardInterrupt as e:
+        print 'zmq_pub quit...'
+        socket.close()
+        sys.exit(e)
+
+
+def zmq_server():
+    try:
+        context = zmq.Context()
+        socket = context.socket(zmq.REP)
+        socket.bind("tcp://127.0.0.1:5001")
+        while 1:
+            msg = socket.recv()
+            print 'zmq_server get msg[%s]' % msg
+            socket.send(msg)
+
+    except KeyboardInterrupt as e:
+        print 'zmq_server quit...'
+        socket.close()
+        sys.exit(e)
+
+
+@contextmanager
+def zmq_pub_socket():
+    try:
+        pub_context = zmq.Context()
+        pub_socket = pub_context.socket(zmq.PUB)
+        pub_socket.bind(ZMQ_ADDR.format(port=PUB_POTR))
+        yield pub_socket
+    finally:
+        print 'ZmqPubServer: pub_socket Exit'
+        pub_socket.close()
+
+
+@contextmanager
+def zmq_rep_socket():
+    try:
+        rep_context = zmq.Context()
+        rep_socket = rep_context.socket(zmq.REP)
+        rep_socket.bind(ZMQ_ADDR.format(port=REP_POTR))
+        yield rep_socket
+    finally:
+        rep_socket.close()
+        print 'ZmqPubServer: rep_socket Exit'
+    pass
+
+class ZmqPubServer(threading.Thread):
+
+    def __init__(self):
+        super(ZmqPubServer, self).__init__()
+
+        self.event = threading.Event()
+        self.reg_event_status = {}
+        self.reg_event_status_last = {}
+        self.reg_event_set = set()
+
+    def event_add_handler(self, handler, *args, **kwargs):
+        handler(*args, **kwargs)
+
+
+    def event_register(self):
+        with zmq_rep_socket() as rep_socket:
+            while 1:
+                msg = rep_socket.recv()
+                print 'zmq_server get msg[%s]' % msg
+                # add by a handler, wait to modify
+                self.reg_event_set.add(msg)
+                rep_socket.send(msg)
+                if msg == ZMQ_EXIT:
+                    break
+
+    def zmq_json_msg(self, d):
+        if isinstance(d, dict):
+            res =json.dumps(d)
+        return res or ''
+
+    # can override here for different work!!!
+    def update_event_status(self):
+        pass
+
+        for item in self.reg_event_set:
+            # modify here
+            self.reg_event_status[item] = time.ctime()
+
+        # how to judage 2 dict
+        if self.reg_event_status_last == self.reg_event_status:
+            return False
+
+        self.reg_event_status_last = self.reg_event_status
+        return True
+
+
+    def run(self):
+        ZMQ_JSON_MSG = lambda x : ZMQ_MSG(self.zmq_json_msg(x))
+        th = threading.Thread(target=self.event_register)
+        th.setDaemon(True)
+        th.start()
+        with zmq_pub_socket() as pub_socket:
+            while 1:
+                # use zmq_pub to publish msg
+                print self.reg_event_status
+                print self.reg_event_status_last
+                # update event status
+                if self.update_event_status():
+                    msg = ZMQ_JSON_MSG(self.reg_event_status)
+                    pub_socket.send(msg)
+
+                time.sleep(1)
+
+
+if __name__ == '__main__':
+    # zmq_server()
+
+    try:
+        ser = ZmqPubServer()
+        # ser.event_register()
+        ser.run()
+    except KeyboardInterrupt:
+        with zmq_req_socket() as req_socket:
+            req_socket.send(ZMQ_EXIT)
+            req_socket.recv()
+        print 'ZmqPubServer: Quit...'
+    pass
 
 
--- mq/zmq_sub.py
+++ mq/zmq_sub.py
@@ -1,13 +1,134 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import sys
 import zmq
+import time
+import json
+import threading
+from contextlib import contextmanager
 
-context = zmq.Context()
-socket = context.socket(zmq.SUB)
-socket.connect("tcp://127.0.0.1:5000")
-socket.setsockopt(zmq.SUBSCRIBE, "zmq")
+SUB_POTR = 5000
+REQ_POTR = 5001
+ZMQ_ADDR = "tcp://127.0.0.1:{port:}"
+ZMQ_PREFIX = r'ZMQ://'
+ZMQ_MSG = lambda x : ZMQ_PREFIX + x
+
+__all__ = ['zmq_sub_socket', 'zmq_req_socket']
 
 
-while 1:
-    print socket.recv()
+def zmq_sub():
+    try:
+        context = zmq.Context()
+        socket = context.socket(zmq.SUB)
+        socket.connect("tcp://127.0.0.1:5000")
+        socket.setsockopt(zmq.SUBSCRIBE, "zmq")
+        while 1:
+            print socket.recv()
+    except KeyboardInterrupt as e:
+        print 'zmq_sub quit...'
+        socket.close()
+        sys.exit(e)
+
+def zmq_client(ci):
+    try:
+        context = zmq.Context()
+        socket = context.socket(zmq.REQ)
+        socket.connect("tcp://127.0.0.1:5001")
+        count = 1
+        while 1:
+            msg = '%s : count[%s]' % (ci, count)
+            socket.send('%s : count[%s]' % (ci, count))
+            req_status =  socket.recv()
+            print msg + ' send result[%s]' % (req_status == msg)
+            count += 1
+            time.sleep (1)
+    except KeyboardInterrupt as e:
+        print 'zmq_sub client...'
+        socket.close()
+        sys.exit(e)
+
+
+@contextmanager
+def zmq_sub_socket():
+    try:
+        sub_context = zmq.Context()
+        sub_socket = sub_context.socket(zmq.SUB)
+        sub_socket.connect(ZMQ_ADDR.format(port=SUB_POTR))
+        sub_socket.setsockopt(zmq.SUBSCRIBE, ZMQ_PREFIX)
+        yield sub_socket
+    finally:
+        print 'ZmqSubClient: sub_socket Exit'
+        sub_socket.close()
+
+
+@contextmanager
+def zmq_req_socket():
+    try:
+        req_context = zmq.Context()
+        req_socket = req_context.socket(zmq.REQ)
+        req_socket.connect(ZMQ_ADDR.format(port=REQ_POTR))
+        yield req_socket
+    finally:
+        print 'ZmqSubClient: req_socket Exit'
+        req_socket.close()
+
+
+class ZmqSubClient(threading.Thread):
+    def __init__(self):
+        super(ZmqSubClient, self).__init__()
+
+        self.reg_event = {}
+
+
+    def add_event_handler(self, event, handler):
+        if not self.reg_event.has_key(event):
+            self.reg_event[event] = []
+
+        self.reg_event[event].append(handler)
+
+    def event_change_handler(self, event):
+        if not isinstance(self.reg_event[event], list):
+            raise TypeError('ZmqSubClient: TypeError %s is not a list' % event)
+
+        for h in self.reg_event[event]:
+            h()
+
+
+    def zmq_message_parser(self, msg):
+        msg = msg.lstrip(ZMQ_PREFIX)
+        try:
+            res = json.loads(msg)
+        except:
+            res = None
+        return res
+
+    def register_event(self, event, handler):
+        with zmq_req_socket() as req_socket:
+            req_socket.send(event)
+            msg = req_socket.recv()
+            if msg == event:
+                self.add_event_handler(event, handler)
+                print 'ZmqSubClient: add event[%s] Success...' % event
+            else:
+                print 'ZmqSubClient: add event[%s] Error...' % event
+
+    def run(self):
+        with zmq_sub_socket() as sub_socket:
+            while 1:
+                msg = sub_socket.recv()
+                print 'ZmqSubClient: get msg[%s]' % msg
+                print self.zmq_message_parser(msg)
+
+            pass
+
+
+if __name__ == '__main__':
+    import sys
+    # zmq_client(sys.argv[1])
+    try:
+        cli = ZmqSubClient()
+        cli.register_event(sys.argv[1], sys.argv[0])
+        cli.run()
+    except KeyboardInterrupt:
+        print 'ZmqSubClient: Quit...'
