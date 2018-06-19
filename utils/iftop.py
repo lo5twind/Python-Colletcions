8cecad3fd175a7cff7d14aefff9633514a2e3459
lo5twind <lo5twind@foxmail.com>
2018-06-19 11:45:18 +0800

    modify: 优化流量统计算法和平滑算法

--- utils/iftop.py
+++ utils/iftop.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+
+"""
+    2016-10-10:
+        Plus all tx/rx as global interface rate
+    2016-10-18:
+        Use interface with smaller number as inter-net
+    2016-10-26:
+        Use just one interface's data of a 2-interface bridge
+    2016-11-04:
+        Get PortMode from Database
+"""
+
 from __future__ import division
 import os
 import psutil
 import time
 import operator
 
-
+SAMPLE_PERIOD = 2 # seconds
 _rx = operator.itemgetter(0)
 _tx = operator.itemgetter(1)
 
 _pre_pre_ns = {}
+_MAX_TRAFFIC = {'ERROR_TYPE': 1000,'1000M': 1000, '10000M': 10000}
 
 def bytes2human(n):
     """
@@ -65,9 +78,9 @@ def get_net_io():
 
 def print_net_speed(ns):
     os.system('clear')
-    print('{:<5}:  {:<16}, {:<16}'.format('IFace','IN','OUT'))
+    print('{:<6}:  {:<16}| {:<16}'.format('IFace','IN','OUT'))
     for vEth in sorted(ns):
-        print('{:<5}:  {:<16}, {:<16}'.format(vEth,ns[vEth]['IN'], ns[vEth]['OUT']))
+        print('{:<6}:  {:<16}| {:<16}'.format(vEth,ns[vEth]['IN'], ns[vEth]['OUT']))
 
 
 def add_unit(i):
@@ -75,82 +88,133 @@ def add_unit(i):
     return round(i, 4) if i < 1 else round(i, 2)
 
 
-def get_net_speed(_pre_io, _pre_t, nat = []):
+def get_net_speed(_pre_io, pre_sp_t, nat=None, bridges=None, portsinfo=None, pre_sp={}, iftype={}):
+    """
+        @param _pre_io[dict]: Rx/Tx Bytes of every interface last time
+        @param pre_sp_t[dict]: last speed update time of every interface
+        @param portsinfo[dict]: interface type, 1: 2:
+    """
     ns = {}
     _ns = {}
     raw_ns = {}
+    post_ns = {}   # save net speed after process
     gin, gout = (0, 0)
     _cur_io = get_net_io()
     _cur_t = time.time()
-    dt = _cur_t - _pre_t
+    cur_sp_t = {}
+    # print _cur_io
+    # print _pre_io
+    # print dt
 
     for vEth in _cur_io:
+        # try:
         if vEth not in _pre_io:
             _pre_io[vEth] = (0, 0)
 
+        if vEth not in pre_sp_t:
+            pre_sp_t[vEth] = tuple([time.time()] * 2)
+
+        if vEth not in pre_sp:
+            pre_sp[vEth] = (0, 0)
+
+        max_sp = _MAX_TRAFFIC.get(iftype.get(vEth, 0), 1000) * 1048576 / 8
+
         """
-            IN  ---> donw
-            OUT ---> up
+            IN  ---> download
+            OUT ---> upload
         """
-        rx = (_rx(_cur_io[vEth]) - _rx(_pre_io[vEth])) / dt
-        tx = (_tx(_cur_io[vEth]) - _tx(_pre_io[vEth])) / dt
 
-        rx = 0 if rx < 0 else rx
-        tx = 0 if tx < 0 else tx
+        # process rx
+        cur_rx_io = _rx(_cur_io[vEth])
+        pre_rx_io = _rx(_pre_io[vEth])
+        pre_rx_t = _rx(pre_sp_t[vEth])
+        cur_rx_t = pre_rx_t
+        pre_rx_sp = _rx(pre_sp[vEth])
+        if abs(_cur_t - pre_rx_t) > SAMPLE_PERIOD: # sample time period
+            if cur_rx_io == pre_rx_io: # IO bytes not change yet
+                # IO does not change in sample period, speed is 0
+                cur_rx_sp = 0
+            else:
+                # IO bytes change
+                cur_rx_sp = (cur_rx_io - pre_rx_io) / abs(_cur_t - pre_rx_t)
+            # update time
+            cur_rx_t = _cur_t
+        else: # during sample period, keep speed
+            cur_rx_sp = pre_rx_sp
+            
+        # result filter
+        if abs(cur_rx_sp - pre_rx_sp) > 0.25 * pre_rx_sp and pre_rx_sp > 0:
+            # cur_rx_sp = 0.25 * (cur_rx_sp - pre_rx_sp) + pre_rx_sp
+            if cur_rx_sp > 0.25 * max_sp or cur_rx_sp < pre_rx_sp:
+                cur_rx_sp = 1.25 * pre_rx_sp if cur_rx_sp > pre_rx_sp else 0.75 * pre_rx_sp
 
-        # save original speed first
-        raw_ns[vEth] = (rx, tx)
-        global _pre_pre_ns
-        if _pre_pre_ns:
-            pre_rx = _rx(_pre_pre_ns[vEth])
-            pre_tx = _tx(_pre_pre_ns[vEth])
+        cur_rx_sp = min(cur_rx_sp, max_sp)
+            
+        # process tx
+        cur_tx_io = _tx(_cur_io[vEth])
+        pre_tx_io = _tx(_pre_io[vEth])
+        pre_tx_t = _tx(pre_sp_t[vEth])
+        cur_tx_t = pre_tx_t
+        pre_tx_sp = _tx(pre_sp[vEth])
+        if abs(_cur_t - pre_tx_t) > SAMPLE_PERIOD: # sample time period
+            if cur_tx_io == pre_tx_io: # IO bytes not change yet
+                # IO does not change in sample period, speed is 0
+                cur_tx_sp = 0
+            else:
+                # IO bytes change
+                cur_tx_sp = (cur_tx_io - pre_tx_io) / abs(_cur_t - pre_tx_t)
+            # update time
+            cur_tx_t = _cur_t
+        else: # during sample period, keep speed
+            cur_tx_sp = pre_tx_sp
+            
+        # result filter
+        if abs(cur_tx_sp - pre_tx_sp) > 0.25 * pre_tx_sp and pre_tx_sp > 0:
+            # cur_tx_sp = 0.25 * (cur_tx_sp - pre_tx_sp) + pre_tx_sp
+            if cur_tx_sp > 0.25 * max_sp or cur_tx_sp < pre_tx_sp:
+                cur_tx_sp = 1.25 * pre_tx_sp if cur_tx_sp > pre_tx_sp else 0.75 * pre_tx_sp
 
-            # fliter
-            if abs(rx - pre_rx) > 0.45 * pre_rx:
-                _r = rx
-                rx = (rx - pre_rx) * 0.25 + pre_rx
-                # if _r == 0 and rx < 0.001:rx = 0
-                # print ('rx change too fast: rx_old={:16.8f}, '
-                #        'rx_new={:16.8f}, pre_rx={:16.8f}').format(_r, rx, pre_rx)
+        cur_tx_sp = min(cur_tx_sp, max_sp)
 
-            if abs(tx - pre_tx) > 0.45 * pre_tx:
-                _t = tx
-                tx = (tx - pre_tx) * 0.25 + pre_tx
-                # if _t == 0 and tx < 0.001:tx = 0
-                # print ('tx change too fast: tx_old={:16.8f}, '
-                #        'tx_new={:16.8f}, pre_tx={:16.8f}').format(_t, tx, pre_tx)
+        # update speed time
+        pre_sp_t[vEth] = (cur_rx_t, cur_tx_t)
+        # update pre_sp
+        pre_sp[vEth] = (cur_rx_sp, cur_tx_sp)
+            
+        # if nat is not None and vEth in nat:
+        if portsinfo is not None and vEth in portsinfo:
+            if str(portsinfo.get(vEth, 0)) == '1':
+                ns[vEth] = {'IN': bits2human(cur_rx_sp * 8),
+                            'OUT': bits2human(cur_tx_sp * 8)}
 
+                _ns[vEth] = {'IN': add_unit(cur_tx_sp * 8),
+                             'OUT': add_unit(cur_rx_sp * 8)}
 
-        # raw_ns[vEth] = (rx, tx)
+                gin += cur_tx_sp * 8
+                gout += cur_rx_sp * 8
 
-        if vEth in nat:
-            ns[vEth] = {'IN': bits2human(rx * 8),
-                        'OUT': bits2human(tx * 8)}
+            # else:
+            # outer-port
+            elif str(portsinfo.get(vEth, 0)) == '2':
+                ns[vEth] = {'IN': bits2human(cur_tx_sp * 8),
+                            'OUT': bits2human(cur_rx_sp * 8)}
 
-            _ns[vEth] = {'IN': add_unit(rx * 8),
-                         'OUT': add_unit(tx * 8)}
-            gin += rx * 8
-            gout += tx * 8
+                _ns[vEth] = {'IN': add_unit(cur_rx_sp * 8),
+                             'OUT': add_unit(cur_tx_sp * 8)}
 
         else:
-            ns[vEth] = {'IN': bits2human(tx * 8),
-                        'OUT': bits2human(rx * 8)}
+            _ns[vEth] = {'IN': add_unit(cur_rx_sp * 8),
+                         'OUT': add_unit(cur_tx_sp * 8)}
+            ns[vEth] = {'IN': bits2human(cur_rx_sp * 8),
+                        'OUT': bits2human(cur_tx_sp * 8)}
 
-            _ns[vEth] = {'IN': add_unit(rx * 8),
-                         'OUT': add_unit(tx * 8)}
-
-        # ns[vEth] = {'IN': (_rx(_cur_io[vEth]) - _rx(_pre_io[vEth])) / dt,
-        #             'OUT': (_tx(_cur_io[vEth]) - _tx(_pre_io[vEth])) / dt}
-
-        pass
 
     _ns['GLOBAL'] = {'IN': add_unit(gin), 'OUT': add_unit(gout)}
+    ns['GLOBAL'] = {'IN': bits2human(gin), 'OUT': bits2human(gout)}
 
     # print _ns
-
-    _pre_pre_ns = raw_ns
-
-    return _cur_io, _cur_t, ns, _ns
+    # print ns
+    return _cur_io, pre_sp_t, ns, _ns
 
 
 class NetIO(object):
@@ -159,11 +223,15 @@ class NetIO(object):
 
 if __name__ == '__main__':
     pre_io = get_net_io()
-    pre_t = time.time()
+    # pre_t = time.time()
+    pre_t = {}
     speed = {}
-    while 1:
-        pre_io, pre_t, speed, _  = get_net_speed(pre_io, pre_t)
-        print_net_speed(speed)
+    try:
+        while 1:
+            pre_io, pre_t, speed, _  = get_net_speed(pre_io, pre_t)
+            print_net_speed(speed)
 
-        time.sleep(2)
+            time.sleep(2)
+    except KeyboardInterrupt:
+        pass
     pass
